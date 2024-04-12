2aed0e7a1f0aa290e932998207115c7161899516
Inandhumano03 <125265388+Inandhumano03@users.noreply.github.com>
2024-04-12 16:14:43 +0530

    Add files via upload
    good

---
+++ health (1)/Healthcare/Healthcare/src/main/java/com/example/Healthcare/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+
+package com.example.Healthcare.repository;
+
+
+import com.example.Healthcare.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductName(String productName);
+    @Query("SELECT p FROM Product p WHERE p.category = ?1")
+    List<Product> findByCategory(String category);
+}
